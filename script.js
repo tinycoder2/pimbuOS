db357efe655a09566f4de39c623a254832676d8a
tinycoder2 <swaathykumar02@gmail.com>
2021-07-24 03:17:01 +0530

    redid layout, added home button , apps and paint iframe

--- script.js
+++ script.js
@@ -1,33 +1,14 @@
-/*
-$(".navbar").hide();
-  $(window).scroll(function () {
-    if ($(window).scrollTop() > 40) {
-      $(".navbar").slideDown(100);
-    } else {
-      $(".navbar").slideUp(100);
-    }
-  });
-  $(".go-down a").click(function (e) {
-    e.preventDefault();
-    var top = $($(this).attr("href")).position().top - 70;
-    $("html, body").animate(
-      {
-        scrollTop: top
-      },
-      1000
-    );
-  });
-  document.querySelector('#popup').style.display = 'none';
-*/
+
 
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+  
+  
 }
 
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
+  
 }
 
 function closePopUp(id) {
@@ -35,3 +16,28 @@ function closePopUp(id) {
   
 }
 
+function toggleMenu() {
+  var x = document.getElementById("myMenu");
+  if (x.style.height == "0%") 
+  {
+    x.style.height = "100%";
+    x.style.width = "30%";
+  } 
+  else 
+  {
+    x.style.height = "0%";
+    x.style.width = "0%";
+  }
+}
+
+function openPaint() {
+  document.getElementById("appRow").style.display = 'none';
+  document.getElementById("paintApp").style.display = 'block';
+  
+}
+
+function closePaint() {
+  document.getElementById("paintApp").style.display = 'none';
+  document.getElementById("appRow").style.display = 'block';
+  
+}
